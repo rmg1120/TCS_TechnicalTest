5e79917d8293d54f11aa528785b2635e619294f2
Roland Gabriel <rolanmgabriel@gmail.com>
2025-09-04 10:56:29 +0800

    Fix: Add DI repositories and services in Program.cs and added .gitignore

--- TCSTest/Program.cs
+++ TCSTest/Program.cs
@@ -1,8 +1,22 @@
+using TCSTest.Models;
+using TCSTest.Repositories;
+using TCSTest.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// DI: repositories
+builder.Services.AddSingleton<IJsonRepository<Channel>, ChannelRepository>();
+builder.Services.AddSingleton<IJsonRepository<ContentItem>, ContentRepository>();
+builder.Services.AddSingleton<IJsonRepository<Schedule>, ScheduleRepository>();
+
+// DI: services
+builder.Services.AddSingleton<IChannelService, ChannelService>();
+builder.Services.AddSingleton<IContentService, ContentService>();
+builder.Services.AddSingleton<IScheduleService, ScheduleService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
